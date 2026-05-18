278128d342318d54b4ae46f69a2bcac8fd846971
Ayala <ayala33081@gmail.com>
2026-05-19 00:11:05 +0300

    Implement file writing for Employee instances and create directory for employee data

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const Employee = require("./classEmployee.js"); 
+const fs = require('node:fs'); 
+const fsPromises = require('node:fs/promises'); 
 
 const emp1 = new Employee(1, "Moshe", 10000);
 const emp2 = new Employee(2, "Yossi", 20000);
 const emp3 = new Employee(3, "Dina", 30000);
+
+fs.mkdirSync('./employees_data');
+
+fs.writeFileSync(`./employees_data/${emp1.empName}.txt`, JSON.stringify(emp1));
+
+fs.writeFile(`./employees_data/${emp2.empName}.txt`, JSON.stringify(emp2), (err) => {
+    if (err) {
+        console.log('שגיאה בשמירת emp2:', err.message);
+    } 
+    else {
+        console.log('emp2 נשמר בהצלחה עם Callback!');
+    }
+});
+
+fsPromises.writeFile(`./employees_data/${emp3.empName}.txt`, JSON.stringify(emp3))
+    .then(() => {
+        console.log('emp3 נשמר בהצלחה  !');
+    })
+    .catch((err) => {
+        console.log('שגיאה בשמירת emp3:', err.message);
+    });
