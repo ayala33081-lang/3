acce1f2a758cbdcfbf2cd9c872d98f68ba5bf6e1
Ayala <ayala33081@gmail.com>
2026-05-18 23:45:22 +0300

    Add initial implementation of Employee instances in index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const Employee = require("./classEmployee.js"); 
+
+const emp1 = new Employee(1, "Moshe", 10000);
+const emp2 = new Employee(2, "Yossi", 20000);
+const emp3 = new Employee(3, "Dina", 30000);
